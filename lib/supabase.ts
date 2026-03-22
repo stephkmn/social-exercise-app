b2eeabaa67fed4fca896ec7b12f1afb413c3e1b9
Aaron Cayanan <aaroncayanan02@gmail.com>
2026-03-22 01:00:42 -0700

    add supabase handling to frontend, get request to home page

---
+++ lib/supabase.ts
@@ -0,0 +1,11 @@
+import 'react-native-url-polyfill/auto';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and/or anon key not provided');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
